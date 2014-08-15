8221d5d05fc406738f8a99e82b2261f2471c1aa1
Daniel Casner <daniel@danielcasner.org>
2014-08-15 12:56:06 -0700

    Create minimal admin interface.

---
+++ admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from models import *
+
+admin.site.register(RGUser)
+admin.site.register(RGClass)
+admin.site.register(RGTraining)
+admin.site.register(RGEquipment)
+admin.site.register(RGCheckins)
